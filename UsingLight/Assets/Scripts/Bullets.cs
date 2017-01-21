55f754ce80f8b05369299031416186f8a09d03f3
Elbis Bolton <elbis.bolton8@gmail.com>
2017-01-21 01:59:01 -0600

    Movement mechanics down, working on firing from all angles

---
+++ UsingLight/Assets/Scripts/Bullets.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour {
+
+
+    float speed = 4.0f;
+    public string PlayerDirection;
+    float timer = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+            Destroy(gameObject);
+        switch(PlayerDirection)
+        {
+            case "NE":
+
+                transform.position += speed * transform.up * Time.deltaTime;
+                transform.position += speed * transform.right * Time.deltaTime;
+                break;
+            case "NW":
+                transform.position += speed * transform.up * Time.deltaTime;
+                transform.position -= speed * transform.right * Time.deltaTime;
+                break;
+            case "SE":
+                transform.position -= speed * transform.up * Time.deltaTime;
+                transform.position += speed * transform.right * Time.deltaTime;
+                break;
+            case "SW":
+                transform.position -= speed * transform.up * Time.deltaTime;
+                transform.position -= speed * transform.right * Time.deltaTime;
+                break;
+            case "N":
+                transform.position += speed * transform.up * Time.deltaTime;
+                break;
+            case "S":
+                transform.position -= speed * transform.up * Time.deltaTime;
+                break;
+            case "W":
+                transform.position -= speed * transform.right * Time.deltaTime;
+                break;
+            case "E":
+                transform.position += speed * transform.right * Time.deltaTime;
+                break;
+            default:
+                Destroy(gameObject);
+                break;
+
+        }
+        
+	}
+}
--- UsingLight/Assets/Scripts/Player.cs
+++ UsingLight/Assets/Scripts/Player.cs
@@ -1,16 +1,97 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-
-	// Use this for initialization
+    
+    //Player properties
+    int health = 100;
+    int stamina = 100;
+    bool canDamage = true; //Always true unless the shield is up or stamina = 0
+    float speed = 2.0f;
+    string playerDirection;
+    public GameObject bullet;
+    Vector3 positionOfBullet;
+    
+    
 	void Start () {
-		
+        
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += speed * transform.up * Time.deltaTime;
+            transform.position -= speed * transform.right * Time.deltaTime;
+            positionOfBullet = new Vector3(transform.position.x - .5f, transform.position.y + .5f, 0);
+            playerDirection = "NW";
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += speed * transform.up * Time.deltaTime;
+            transform.position += speed * transform.right * Time.deltaTime;
+            positionOfBullet = new Vector3(transform.position.x + .5f, transform.position.y + .5f, 0);
+            playerDirection = "NE";
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= speed * transform.up * Time.deltaTime;
+            transform.position -= speed * transform.right * Time.deltaTime;
+            positionOfBullet = new Vector3(transform.position.x - .5f, transform.position.y - .5f, 0);
+            playerDirection = "SW";
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position -= speed * transform.up * Time.deltaTime;
+            transform.position += speed * transform.right * Time.deltaTime;
+            positionOfBullet = new Vector3(transform.position.x + .5f, transform.position.y - .5f, 0);
+            playerDirection = "SE";
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //transform.Translate((1) * Camera.main.transform.forward * Time.deltaTime);
+            transform.position += speed * transform.up * Time.deltaTime;
+            positionOfBullet = new Vector3(transform.position.x, transform.position.y + .5f, 0);
+            playerDirection = "N";
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //transform.Translate((-1) * Camera.main.transform.forward * Time.deltaTime);
+            transform.position -= speed * transform.up * Time.deltaTime;
+            positionOfBullet = new Vector3(transform.position.x, transform.position.y - .5f, 0);
+
+            playerDirection = "S";
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //transform.Translate((-1) * Camera.main.transform.right * Time.deltaTime);
+            transform.position -= speed * transform.right * Time.deltaTime;
+            positionOfBullet = new Vector3(transform.position.x - .5f, transform.position.y, 0);
+
+            playerDirection = "W";
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //transform.Translate((1) * Camera.main.transform.right * Time.deltaTime);
+            transform.position += speed * transform.right * Time.deltaTime;
+            positionOfBullet = new Vector3(transform.position.x + .5f, transform.position.y, 0);
+            playerDirection = "E";
+        }
+
+        if(Input.GetKeyUp(KeyCode.Z))
+        {
+            bullet.GetComponent<Bullets>().PlayerDirection = playerDirection;
+            Instantiate(bullet, positionOfBullet, transform.rotation);
+
+
+        }
+
+
+
+    }
+
+    
 }
