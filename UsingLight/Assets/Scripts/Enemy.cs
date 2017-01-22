b5e6dd2f18405a48b2cf23604502a410b996aa93
Elbis Bolton <elbis.bolton8@gmail.com>
2017-01-21 23:25:50 -0600

    player walk/left/right animator. But has bug does not run correct

--- UsingLight/Assets/Scripts/Enemy.cs
+++ UsingLight/Assets/Scripts/Enemy.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    int health = 100;
+    int hitdamage;
+    float distanceToPlayer;
+    float speed = 2.0f;
+    GameObject Player;
+
 	// Use this for initialization
 	void Start () {
-		
+        //Find player object 
+        Player = GameObject.FindObjectOfType<Player>().gameObject;  
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        distanceToPlayer = Vector2.Distance(Player.transform.position, gameObject.transform.position);
+
+        if (distanceToPlayer < 5f)
+        {
+            //Start moving towards the player 
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, Player.transform.position, speed * Time.deltaTime);
+        }
+
+        if(distanceToPlayer < 1)
+        {
+            //Start attacking the player
+
+        }
+
+
 	}
 }
--- UsingLight/Assets/Scripts/Player.cs
+++ UsingLight/Assets/Scripts/Player.cs
@@ -4,16 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+
+
+    public enum PlayerDirection
+    {
+        N, S, E, W, NE, NW, SE, SW
+    }
+    
     
     //Player properties
     int health = 100;
     int stamina = 100;
     bool canDamage = true; //Always true unless the shield is up or stamina = 0
     float speed = 2.0f;
-    string playerDirection;
     public GameObject bullet;
     Vector3 positionOfBullet;
-    
+    public Animator animator; 
+    PlayerDirection playerDirection;
+    string currentAnimation;
     
 	void Start () {
         
@@ -21,77 +29,91 @@ public class Player : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
+
+        
+
         if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
         {
+            
             transform.position += speed * transform.up * Time.deltaTime;
             transform.position -= speed * transform.right * Time.deltaTime;
             positionOfBullet = new Vector3(transform.position.x - .5f, transform.position.y + .5f, 0);
-            playerDirection = "NW";
+            playerDirection = PlayerDirection.NW;
         }
         else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += speed * transform.up * Time.deltaTime;
             transform.position += speed * transform.right * Time.deltaTime;
             positionOfBullet = new Vector3(transform.position.x + .5f, transform.position.y + .5f, 0);
-            playerDirection = "NE";
+            playerDirection = PlayerDirection.NE;
         }
         else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position -= speed * transform.up * Time.deltaTime;
             transform.position -= speed * transform.right * Time.deltaTime;
             positionOfBullet = new Vector3(transform.position.x - .5f, transform.position.y - .5f, 0);
-            playerDirection = "SW";
+            playerDirection = PlayerDirection.SW;
         }
         else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow))
         {
             transform.position -= speed * transform.up * Time.deltaTime;
             transform.position += speed * transform.right * Time.deltaTime;
             positionOfBullet = new Vector3(transform.position.x + .5f, transform.position.y - .5f, 0);
-            playerDirection = "SE";
+            playerDirection = PlayerDirection.SE;
         }
         else if (Input.GetKey(KeyCode.UpArrow))
         {
+            animator.Play("PlayerWalk");
             //transform.Translate((1) * Camera.main.transform.forward * Time.deltaTime);
             transform.position += speed * transform.up * Time.deltaTime;
             positionOfBullet = new Vector3(transform.position.x, transform.position.y + .5f, 0);
-            playerDirection = "N";
+            playerDirection = PlayerDirection.N;
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
             //transform.Translate((-1) * Camera.main.transform.forward * Time.deltaTime);
             transform.position -= speed * transform.up * Time.deltaTime;
             positionOfBullet = new Vector3(transform.position.x, transform.position.y - .5f, 0);
-
-            playerDirection = "S";
+            playerDirection = PlayerDirection.S;
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
+            animator.Play("WalkLeft");
             //transform.Translate((-1) * Camera.main.transform.right * Time.deltaTime);
             transform.position -= speed * transform.right * Time.deltaTime;
             positionOfBullet = new Vector3(transform.position.x - .5f, transform.position.y, 0);
-
-            playerDirection = "W";
+            playerDirection = PlayerDirection.W;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
+            animator.Play("WalkRight");
             //transform.Translate((1) * Camera.main.transform.right * Time.deltaTime);
             transform.position += speed * transform.right * Time.deltaTime;
             positionOfBullet = new Vector3(transform.position.x + .5f, transform.position.y, 0);
-            playerDirection = "E";
+            playerDirection = PlayerDirection.E;
         }
 
         if(Input.GetKeyUp(KeyCode.Z))
         {
-            bullet.GetComponent<Bullets>().PlayerDirection = playerDirection;
+            bullet.GetComponent<Bullets>().PlayerDirection = playerDirection.ToString();
             Instantiate(bullet, positionOfBullet, transform.rotation);
-
-
         }
 
 
 
     }
+    void TransitionAnimation(string animationName)
+    {
+        if (currentAnimation != animationName)
+        {
+            animator.Play(animationName);
+            currentAnimation = animationName;
+        }
+
+    }
+
+    
 
     
 }
